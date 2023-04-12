2f2d38e88de1a7f1ebf5a8586138d815b5e65f59
moonjongseong <109961492+moonjongseong@users.noreply.github.com>
2023-04-12 23:28:37 +0900

    feat: 설정 화면 UI를 구현한다 (#13)
    * feat: 설정 UI 설계
    
    * setting: 환경설정 변경

---
+++ app/src/main/java/com/example/bravohealthpark/PreferenceActivity.java
@@ -0,0 +1,12 @@
+package com.example.bravohealthpark;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import com.example.myapplication.R;
+public class PreferenceActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_preference);
+    }
+}
