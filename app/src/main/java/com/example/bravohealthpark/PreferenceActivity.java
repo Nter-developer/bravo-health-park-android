1813bad542efc1e5843f9e5b3a3d4aad10154fcc
moon <unravel1129@gmail.com>
2023-04-12 22:45:01 +0900

    feat: 설정 UI 설계

---
+++ app/src/main/java/com/example/bravohealthpark/PreferenceActivity.java
@@ -0,0 +1,14 @@
+package com.example.bravohealthpark;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PreferenceActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_preference);
+    }
+}
