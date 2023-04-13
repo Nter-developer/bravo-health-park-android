707b5753dc25b4c7e55a4d23d88b28f49562b81a
moon <unravel1129@gmail.com>
2023-04-13 22:16:23 +0900

    feat: 소리 설정 UI 설계

---
+++ app/src/main/java/com/example/bravohealthpark/PrefSoundActivity.java
@@ -0,0 +1,17 @@
+package com.example.bravohealthpark;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ImageView;
+
+import com.example.myapplication.R;
+
+public class PrefSoundActivity extends AppCompatActivity {
+    private ImageView imageview_button;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pref_sound);
+    }
+}
