a7adc9b439312c0f470b747a96aa8ea73bb9024b
Yi Sanghyun <59863112+Nter-developer@users.noreply.github.com>
2023-04-22 21:31:26 +0900

    feat: 어플, 산학협력 업체, KGU 로고 및 파일명을 수정한다 (#17)
    * feat: res/ 이미지 파일 배경 제거
    
    * setting: launcher icon 변경
    
    * refactor: layout/*.xml error 요소 제거

--- app/src/main/java/com/example/bravohealthpark/AdapterActivity.java
+++ app/src/main/java/com/example/bravohealthpark/AdapterActivity.java
@@ -5,7 +5,6 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.TextView;
-import com.example.myapplication.R;
 import java.util.ArrayList;
 
 public class AdapterActivity extends BaseAdapter {
