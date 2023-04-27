990d6b33da47fe5cb4c73a4b8e88b912a10d71b2
Sanghyun Yi <njsharry32@gmail.com>
2023-04-28 02:03:52 +0900

    refactor: SignUp xml, Activity 수정

--- app/src/main/java/com/example/bravohealthpark/SignUpActivity.java
+++ app/src/main/java/com/example/bravohealthpark/SignUpActivity.java
@@ -1,23 +1,24 @@
 package com.example.bravohealthpark;
 
-import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-public class SignupActivity extends AppCompatActivity {
+import androidx.appcompat.app.AppCompatActivity;
 
-    private Button mSignupBtn;
+public class SignUpActivity extends AppCompatActivity {
+
+    private Button signUpBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
 
-        mSignupBtn = (Button) findViewById(R.id.signup_button_2);
+        signUpBtn = (Button) findViewById(R.id.button_signup);
 
-        mSignupBtn.setOnClickListener(new View.OnClickListener() {
+        signUpBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
