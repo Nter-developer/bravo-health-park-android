7b089ee830750619c15fe7211ed062153b2d5293
moonjongseong <109961492+moonjongseong@users.noreply.github.com>
2023-05-07 18:31:39 +0900

    fix: 표기법을 통일하고 불필요한 코드를 정리한다. (#23)
    * LoginActivity, Activity_login 표기법 수정 완료
    
    * PrefSoundActivity 불필요 코드 삭제
    
    * activity_pref_sound 불필요 코드 삭제 및 표기법 수정 완료
    
    * activity_profile 표기법 수정 완료
    
    * activity.xml파스칼표기법 으로 통일 완료, Activity.java카멜 표기법으로 통일 완료

--- app/src/main/java/com/example/bravohealthpark/PrefSoundActivity.java
+++ app/src/main/java/com/example/bravohealthpark/PrefSoundActivity.java
@@ -6,8 +6,6 @@ import android.widget.ImageView;
 
 public class PrefSoundActivity extends AppCompatActivity {
 
-    private ImageView imageviewBtn;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
