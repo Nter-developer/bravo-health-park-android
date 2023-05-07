6f26ae2d26dd796638ac5230899a702e76a85434
moonjongseong <109961492+moonjongseong@users.noreply.github.com>
2023-05-07 17:04:46 +0900

    회원가입 UI 수정 완료 (#22)

--- app/src/main/java/com/example/bravohealthpark/SignUpActivity.java
+++ app/src/main/java/com/example/bravohealthpark/SignUpActivity.java
@@ -1,11 +1,12 @@
 package com.example.bravohealthpark;
 
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-
-import androidx.appcompat.app.AppCompatActivity;
 
 public class SignUpActivity extends AppCompatActivity {
 
@@ -14,9 +15,9 @@ public class SignUpActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_signup);
+        setContentView(R.layout.activity_sign_up);
 
-        signUpBtn = (Button) findViewById(R.id.button_signup);
+        signUpBtn = (Button) findViewById(R.id.SignUp_Btn);
 
         signUpBtn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -25,5 +26,6 @@ public class SignUpActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
     }
 }
