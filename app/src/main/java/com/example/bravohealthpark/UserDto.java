b395390c136a0b9d8ccf2ef65b50c1bf9040b2f7
Sanghyun Yi <njsharry32@gmail.com>
2023-05-28 09:01:46 +0900

    feat: 회원가입 DTO 추가

---
+++ app/src/main/java/com/example/bravohealthpark/UserDto.java
@@ -0,0 +1,30 @@
+package com.example.bravohealthpark;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class UserDto {
+    private Set<AuthorityDto> authorityDtoSet;
+    private String loginId;
+    private String phoneNumber;
+    private String username;
+
+    public UserDto(HashSet<AuthorityDto> authorityDtoSet, String loginId, String phoneNumber, String username) {
+        this.authorityDtoSet = authorityDtoSet;
+        this.loginId = loginId;
+        this.phoneNumber = phoneNumber;
+        this.username = username;
+    }
+
+    public void setLoginId(String loginId) {
+        this.loginId = loginId;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+}
