29a73fb12e0fa112867b2d800761857e49c522e8
Sanghyun Yi <njsharry32@gmail.com>
2023-05-31 14:27:39 +0900

    feat: 로그인 통신 메소드 추가  및 쿠키 Intercept 적용

--- app/src/main/java/com/example/bravohealthpark/RetrofitService.java
+++ app/src/main/java/com/example/bravohealthpark/RetrofitService.java
@@ -2,6 +2,8 @@ package com.example.bravohealthpark;
 
 import com.example.bravohealthpark.SignupResult;
 import com.example.bravohealthpark.UserDto;
+import com.example.bravohealthpark.retrofit.LoginDto;
+import com.example.bravohealthpark.retrofit.LoginResponse;
 
 import retrofit2.Call;
 import retrofit2.http.Body;
@@ -10,4 +12,7 @@ import retrofit2.http.POST;
 public interface RetrofitService {
     @POST("/user/signup")
     Call<SignupResult> sendSignupRequest(@Body UserDto userDto);
+
+    @POST("/user/login")
+    Call<LoginResponse> sendLoginRequest(@Body LoginDto loginDto);
 }
