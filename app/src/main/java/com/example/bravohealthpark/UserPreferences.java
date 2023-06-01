73f1bf42d8fff2ac91cd8034f838598992aea4be
Sanghyun Yi <njsharry32@gmail.com>
2023-06-01 21:58:06 +0900

    feat: Preference Key 값 저장을 위한 UserPreferences 구현

---
+++ app/src/main/java/com/example/bravohealthpark/UserPreferences.java
@@ -0,0 +1,8 @@
+package com.example.bravohealthpark;
+
+public class UserPreferences {
+
+    public static final String SHARED_PREFERENCE_USER_ID = "UserIdPreferences";
+    public static final String SHARED_PREFERENCE_USER_PNUMBER = "UserPhoneNumberPreferences";
+    public static final String SHARED_PREFERENCE_USER_NAME = "UserNamePreferences";
+}
