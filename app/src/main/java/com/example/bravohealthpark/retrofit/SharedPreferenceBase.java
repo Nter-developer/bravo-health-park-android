7a10e1f03921a580cb93ede18ecf68ad5b3ecd39
Yi Sanghyun <59863112+Nter-developer@users.noreply.github.com>
2023-06-02 00:21:24 +0900

    refactor: 저장한 토큰으로 자동 로그인하도록 로직을 변경한다  (#45)
    * feat: Preference Key 값 저장을 위한 UserPreferences 구현
    
    * feat: 토스트 메시지 명세를 위한 CustomToastMessage 구현
    
    * refactor: Custrom SharedPreference에 String value 메소드 추가
    
    * feat: LoginResponse getter 메소드 추가
    
    * refactor: LoginActivity 토큰을 이용한 자동 로그인 로직으로 변경

--- app/src/main/java/com/example/bravohealthpark/retrofit/SharedPreferenceBase.java
+++ app/src/main/java/com/example/bravohealthpark/retrofit/SharedPreferenceBase.java
@@ -9,7 +9,25 @@ public class SharedPreferenceBase {
 
     private static SharedPreferences sharedPreferences;
 
-    public static void setSharedPreference(String key, Set<String> value) {
+    public static void setSharedPreference(String key, String value) {
+        if (sharedPreferences == null) {
+            // Error handling or initialization logic if needed
+            return;
+        }
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putString(key, value);
+        editor.apply();
+    }
+
+    public static String getSharedPreference(String key, String defaultValue) {
+        if (sharedPreferences == null) {
+            // Error handling or initialization logic if needed
+            return null;
+        }
+        return sharedPreferences.getString(key, defaultValue);
+    }
+
+    public static void setSharedPreferenceSet(String key, Set<String> value) {
         if (sharedPreferences == null) {
             // Error handling or initialization logic if needed
             return;
@@ -19,7 +37,7 @@ public class SharedPreferenceBase {
         editor.apply();
     }
 
-    public static Set<String> getSharedPreference(String key, Set<String> defaultValue) {
+    public static Set<String> getSharedPreferenceSet(String key, Set<String> defaultValue) {
         if (sharedPreferences == null) {
             // Error handling or initialization logic if needed
             return defaultValue;
