c42b7fdda7234d2ef5fa1214f106d4eed1355bb6
Sanghyun Yi <njsharry32@gmail.com>
2023-06-02 20:33:41 +0900

    refactor: SharedPreferenceBase 사용하지 않는 함수 제거

--- app/src/main/java/com/example/bravohealthpark/retrofit/SharedPreferenceBase.java
+++ app/src/main/java/com/example/bravohealthpark/retrofit/SharedPreferenceBase.java
@@ -3,8 +3,6 @@ package com.example.bravohealthpark.retrofit;
 import android.content.Context;
 import android.content.SharedPreferences;
 
-import java.util.Set;
-
 public class SharedPreferenceBase {
 
     private static SharedPreferences sharedPreferences;
@@ -21,28 +19,9 @@ public class SharedPreferenceBase {
 
     public static String getSharedPreference(String key, String defaultValue) {
         if (sharedPreferences == null) {
-            // Error handling or initialization logic if needed
             return null;
         }
         return sharedPreferences.getString(key, defaultValue);
-    }
-
-    public static void setSharedPreferenceSet(String key, Set<String> value) {
-        if (sharedPreferences == null) {
-            // Error handling or initialization logic if needed
-            return;
-        }
-        SharedPreferences.Editor editor = sharedPreferences.edit();
-        editor.putStringSet(key, value);
-        editor.apply();
-    }
-
-    public static Set<String> getSharedPreferenceSet(String key, Set<String> defaultValue) {
-        if (sharedPreferences == null) {
-            // Error handling or initialization logic if needed
-            return defaultValue;
-        }
-        return sharedPreferences.getStringSet(key, defaultValue);
     }
 
     public static void initialize(Context context) {
