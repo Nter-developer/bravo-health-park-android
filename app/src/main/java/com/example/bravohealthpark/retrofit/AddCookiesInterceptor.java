5c63ef1620d443408f09a6f1067d734d66082dab
Sanghyun Yi <njsharry32@gmail.com>
2023-06-02 20:44:24 +0900

    refactor: 토큰 발급시 List에서 String으로 변환해 저장하도록 수정

--- app/src/main/java/com/example/bravohealthpark/retrofit/AddCookiesInterceptor.java
+++ app/src/main/java/com/example/bravohealthpark/retrofit/AddCookiesInterceptor.java
@@ -1,39 +1,24 @@
 package com.example.bravohealthpark.retrofit;
 
-import android.content.Context;
-
 import java.io.IOException;
-import java.util.HashSet;
-import java.util.Set;
-
 import okhttp3.Interceptor;
 import okhttp3.Request;
 import okhttp3.Response;
 
 public class AddCookiesInterceptor implements Interceptor {
 
-    private static Context context;
-
-    public Interceptor setContext(Context context) {
-        this.context = context;
-        return this;
-    }
-
     @Override
     public Response intercept(Chain chain) throws IOException {
         Request.Builder builder = chain.request().newBuilder();
 
-        // Preference에서 cookies를 가져오는 작업을 수행
-        Set<String> preferences =  SharedPreferenceBase.getSharedPreferenceSet(
-                APIPreferences.SHARED_PREFERENCE_NAME_COOKIE, new HashSet<String>());
+        // Preference에서 token를 가져오는 작업을 수행
+        String token =  SharedPreferenceBase.getSharedPreference(
+                APIPreferences.SHARED_PREFERENCE_NAME_COOKIE, new String());
 
-        for (String cookie : preferences) {
-            builder.addHeader("Cookie", cookie);
-        }
+        builder.addHeader("Authorization", token);
 
         // Web,Android,iOS 구분을 위해 User-Agent세팅
         builder.removeHeader("User-Agent").addHeader("User-Agent", "Android");
-
 
         return chain.proceed(builder.build());
     }
