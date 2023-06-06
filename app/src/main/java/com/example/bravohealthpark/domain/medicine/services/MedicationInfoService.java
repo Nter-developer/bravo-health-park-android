64f0e9bc2710a8afdfdc1c0e15c87e82a26cfbee
Sanghyun Yi <njsharry32@gmail.com>
2023-06-06 11:36:02 +0900

    feat: 약 정보 Service 인터페이스 생성

---
+++ app/src/main/java/com/example/bravohealthpark/domain/medicine/services/MedicationInfoService.java
@@ -0,0 +1,16 @@
+package com.example.bravohealthpark.domain.medicine.services;
+
+import com.example.bravohealthpark.domain.medicine.domain.dto.SaveMediInfoRequest;
+import com.example.bravohealthpark.domain.medicine.domain.dto.SaveMediInfoResponse;
+
+import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.POST;
+import retrofit2.http.Path;
+
+public interface MedicationInfoService {
+    @POST("/medicationInfo/{userId}")
+    Call<SaveMediInfoResponse> saveMedicationInfo(
+            @Path("userId") Integer userId,
+            @Body SaveMediInfoRequest saveMediInfoRequest);
+}
