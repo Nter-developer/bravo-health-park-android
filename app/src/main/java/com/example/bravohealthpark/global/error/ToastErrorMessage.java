e09b7668d3277906ae27fa0b8fb3fa20600ae966
Sanghyun Yi <njsharry32@gmail.com>
2023-06-06 16:24:23 +0900

    refactor: 커스텀 에러 메시지 클래스 rename

---
+++ app/src/main/java/com/example/bravohealthpark/global/error/ToastErrorMessage.java
@@ -0,0 +1,11 @@
+package com.example.bravohealthpark.global.error;
+
+public class ToastErrorMessage {
+    public static final String ERROR_MESSAGE_NETWORK_ERROR = "네트워크 통신이 불안정합니다";
+    public static final String ERROR_MESSAGE_LOGIN_FAIL = "아이디와 비밀번호를 확인해주세요";
+    public static final String MESSAGE_LOGIN_SUCCESS = "님 환영합니다";
+    public static final String MESSAGE_SIGNUP_SUCCESS = "회원가입에 성공했습니다";
+    public static final String MESSAGE_SIGNUP_FAIL = "회원가입에 실패하였습니다";
+    public static final String MEDI_REGISTER_FAIL = "약 정보를 등록하는데 실패했습니다";
+    public static final String MEDI_REGISTER_SUCCESS = "약 정보 등록에 성공했습니다";
+}
