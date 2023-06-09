7af11364af32a7804eff93f5b3e124a6481e0344
Sanghyun Yi <njsharry32@gmail.com>
2023-06-09 23:55:28 +0900

    feat: 처방전 촬영을 위한 Camera Fragment 생성

---
+++ app/src/main/java/com/example/bravohealthpark/presentation/fragments/CameraFragment.java
@@ -0,0 +1,21 @@
+package com.example.bravohealthpark.presentation.fragments;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.bravohealthpark.R;
+
+public class CameraFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_camera, container, false);
+
+        return v;
+    }
+}
