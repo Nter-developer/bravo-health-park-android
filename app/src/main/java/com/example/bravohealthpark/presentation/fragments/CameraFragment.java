01be7bbcfc8de4122cee8e8a2d63734fdc073325
Sanghyun Yi <njsharry32@gmail.com>
2023-06-10 13:30:49 +0900

    feat: 카메라 호출 및 Camera Fragment 연결

--- app/src/main/java/com/example/bravohealthpark/presentation/fragments/CameraFragment.java
+++ app/src/main/java/com/example/bravohealthpark/presentation/fragments/CameraFragment.java
@@ -1,20 +1,59 @@
 package com.example.bravohealthpark.presentation.fragments;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
+import android.provider.MediaStore;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.fragment.app.Fragment;
 
 import com.example.bravohealthpark.R;
 
 public class CameraFragment extends Fragment {
+    private static final int CAMERA_PERMISSION_REQUEST_CODE = 100;
+    private static final int CAMERA_REQUEST_CODE = 101;
+
+    private Button captureButton;
+    private ActivityResultLauncher<Intent> launcher;
+    private ImageView imageViewCapture;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        launcher = registerForActivityResult(
+                new ActivityResultContracts.StartActivityForResult(),
+                result -> {
+                    if (result.getResultCode() == Activity.RESULT_OK) {
+                        Intent data = result.getData();
+                        Bundle extras = data.getExtras();
+                        Bitmap imageBitmap = (Bitmap) extras.get("data");
+                        imageViewCapture.setImageBitmap(imageBitmap);
+                    } else {
+                        // if fail
+                    }
+                }
+        );
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_camera, container, false);
+
+        imageViewCapture = v.findViewById(R.id.imageView_capture);
+        captureButton = v.findViewById(R.id.capture_button);
+        captureButton.setOnClickListener(view -> {
+            Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+            launcher.launch(intent);
+        });
 
         return v;
     }
